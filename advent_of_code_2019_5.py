de4b1376c052cbadd5815af358ced13d0fef59a7
almyki <50028772+almyki@users.noreply.github.com>
2019-12-07 18:10:01 -0500

    No THIS is the Day 5b results

--- advent_of_code_2019_5.py
+++ advent_of_code_2019_5.py
@@ -2,37 +2,35 @@
 filename = 'AdventOfCode_2019_5.txt'
 
 with open(filename) as f:
-    codes = [int(code) for line in f for code in line.strip().split(',')]
-
-code_block = codes
+    codes_list = [int(code) for line in f for code in line.strip().split(',')]
 
 class IntcodeComputer():
     """Process codes into code blocks and carry out operations."""
     
     def __init__(self, codes):
         self.codes = codes
-        self.code_blocks = self.get_code_blocks()
+        self.outputs = []
 
-    def get_code_blocks(self):
-        """Get the code block, sized based on opcode."""
-        x, pass_block, code_blocks = 0, 0, []
-
-        for code in self.codes:
-            opcode = self.get_opcode(code)
-            if pass_block > 0: 
-                pass_block -= 1
-            elif opcode == 1 or opcode == 2:
-                code_blocks.append(self.codes[x:x+4])
-                print(self.codes[x:x+4])
-                pass_block = 3
-            elif opcode == 3 or opcode == 4:
-                code_blocks.append(self.codes[x:x+2])
-                print(self.codes[x:x+2])
-                pass_block = 1
-            x += 1
-        print('Get Code Blocks: ')
-        print(code_blocks)
-        return code_blocks
+    def get_code_block(self, pos, opcode):
+        """Get code block based on position in codes, sized based on opcode."""
+        if opcode == 99:
+            print('99 IS THE END')
+            block_size = 0
+            return [self.codes[pos]]
+        elif opcode in (1, 2, 7, 8):
+            block_size = 4
+        elif opcode in (5, 6):
+            block_size = 3
+        elif opcode in (3, 4):
+            block_size = 2
+        else:
+            print('OPCODE')
+            print(opcode)
+        code_block = self.codes[pos:pos+block_size]
+        print(f'Pos: {str(pos)} \n{str(pos+block_size)}')
+        print('Get a Code Block: ')
+        print(code_block)
+        return code_block
 
     def get_opcode(self, code):
         """Get the opcode string from a codeblock."""
@@ -42,13 +40,15 @@ class IntcodeComputer():
     def get_modes(self, code_block):
         """Get the two modes for a code block's parameters."""
         # FUCK YOU INDEX ERRORS, LIST COMPS, AND EVEN YOU LAMBDAS I DON'T NEED PRETTY
+        # 0 = pos mode
+        # 1 = imm mode
         modes, mode_codes = [0, 0], list(reversed(str(code_block[0])))[2:]
         x = 0
         for mode in mode_codes:
             modes[x] = int(mode)
             x += 1
-        #print('Get modes: ')
-        #print(modes)
+        print('Get modes: ')
+        print(modes)
         return modes
 
     def get_values(self, code_block):
@@ -66,44 +66,107 @@ class IntcodeComputer():
                 values.append(int(code_block[x]))
             else: print('Error: Not a valid mode.')
             x += 1
-        #print('Get values: ')
-        #print(values)
+        print('Get values: ')
+        print(values)
         return values 
 
-    def get_codes_outputs(self, opcode_3_input=1):
-        """Carry out the operations for the code."""
-        #print('\nStart Getting Codes Outputs for: ')
-        #print(self.codes)
-        outputs = []
-        for code_block in self.code_blocks:
-            opcode = self.get_opcode(code_block[0])
-            #print(f'Get opcode: {str(opcode)}')
-            pos = code_block[-1]
-            # Opcode 99 ends the program.
-            if opcode == 99: break
-            # Opcode 1 adds value 1 and 2, then puts it in pos 3.
-            elif opcode == 1:
-                values = self.get_values(code_block)
-                self.codes[pos] = values[0] + values[1]
-            # Opcode 2 multiplies value 1 and 2, then puts it in pos 3.
-            elif opcode == 2:
-                values = self.get_values(code_block)
-                self.codes[pos] = values[0] * values[1]
-            # Opcode 3 places an input into pos 3.
-            elif opcode == 3:
-                self.codes[pos] = opcode_3_input
-            # Opcode 4 outputs the number at pos 3 to an outputs list.
-            elif opcode == 4:
-                outputs.append(self.codes[pos])
-                if self.codes[pos] != 0:
-                    return outputs
-            # If an opcode is invalid, the program went wrong.
+    def do_operation_get_pos(self, pos, code_block, opcode_input=5):
+        opcode = self.get_opcode(code_block[0])
+        modes = self.get_modes(code_block)
+        if opcode == 99:
+            print('We have reached THE END.')
+            return # ends the program.
+        elif opcode == 1: # adds value 1 and 2, then puts it in pos 3.
+            values = self.get_values(code_block)
+            self.codes[code_block[3]] = values[0] + values[1]
+            print('The values being added together, then the result of adding them in pos 3: ')
+            print(values)
+            print(self.codes[code_block[3]])
+            return pos + 4
+        elif opcode == 2: # multiplies value 1 and 2, then puts it in pos 3.
+            values = self.get_values(code_block)
+            self.codes[code_block[3]] = values[0] * values[1]
+            print('The values being multiplied together, then the result of multiplying them in pos 3: ')
+            print(values)
+            print(self.codes[code_block[3]])
+            return pos + 4
+        elif opcode == 3: # places an input into pos 1.
+            self.codes[code_block[1]] = opcode_input
+            #assert("This should be " + str(opcode_input) + ': ' + str(self.codes[code_block[1]]))
+            return pos + 2
+        elif opcode == 4: # outputs the number at pos 1 or imm 1 to an outputs list.
+            if modes[0] == 0:
+                output = self.codes[code_block[1]]
             else:
-                print('The opcode is wrong.')
+                output = code_block[1]
+            self.outputs.append(output)
+            # If the output != 0, the program ends and returns the output.
+            if output != 0:
+                finish_msg = f'THE FINAL OUTPUT IS: {str(output)}. \nTotal outputs: '
+                print(self.outputs)
+                return finish_msg
+            return pos + 2
 
-puzzle_input = IntcodeComputer(code_block)
-outputs = puzzle_input.get_codes_outputs()
+        # jump-if-true. If param 1 != 0, jump to the pos of param2 else pass.
+        elif opcode == 5:
+            # assert pos not in len(self.codes), "testing assert and opcode 5"
+            print('\nthe position after checking if it\'s opcode 5')
+            print(pos)
+            values = self.get_values(code_block)
+            if values[0] != 0:
+                pos = values[1]
+                return pos
+            else:
+                return pos + 3
+        # jump-if-false. If param1 == 0, jump to pos of param2 else pass.
+        elif opcode == 6:
+            values = self.get_values(code_block)
+            if values[0] == 0:
+                pos = values[1]
+                return pos
+            else:
+                return pos + 3
+        # less than. if param1 < param2, store 1 in pos of param3 else store 0
+        elif opcode == 7:
+            values = self.get_values(code_block)
+            if values[0] < values[1]:
+                self.codes[code_block[3]] = 1
+            else:
+                self.codes[code_block[3]] = 0
+            return pos + 4
+        # equals. If param1 == param2, store 1 in pos of param3 else store 0.
+        elif opcode == 8:
+            values = self.get_values(code_block)
+            if values[0] == values[1]:
+                self.codes[code_block[3]] = 1
+            else:
+                self.codes[code_block[3]] = 0
+            return pos + 4
+        else: #If an opcode is invalid, the program went wrong.
+            print('The opcode is wrong.')
 
-print('\nOutputs: ')
-print(outputs)
+
+
+puzzle_input = IntcodeComputer(codes_list)
+pos = 0
+opcode = puzzle_input.get_opcode(puzzle_input.codes[0])
+
+print('codes list length')
+print(len(codes_list))
+print(puzzle_input.codes[6])
+
+while True:
+    # Get code block starting at pos.
+    code_block = puzzle_input.get_code_block(pos, opcode)
+    
+    # Perform operations on this code block and return next pos.
+    pos = puzzle_input.do_operation_get_pos(pos, code_block)
+    if type(pos) != int:
+        print(code_block)
+        print(pos)
+        break
+
+    # Get the opcode for the next code block from the number at new pos.
+    opcode = puzzle_input.get_opcode(puzzle_input.codes[pos])
+    print(puzzle_input.outputs)
 
